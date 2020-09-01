b23589d9f991361e37bf8da51d213d0d9476d87e
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-31 23:08:59 -0400

    create songdetail component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SongList from './SongList';
+import SongDetail from './SongDetail';
 
 // Reducers specifiy how the application's state changes
 // in response to actions sent to the store
@@ -18,6 +19,9 @@ const App = () => {
       <div className="ui row">
         <div className="column eight wide">
           <SongList />
+        </div>
+        <div className="column eight wide">
+          <SongDetail />
         </div>
       </div>
     </div>
